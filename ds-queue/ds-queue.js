f9a095f7d5ec48c14eb6b2e0c057966c8dc789c5
rudenick0309 <rudenick0309@gmail.com>
2020-04-01 14:39:13 +0900

    que cpt

--- ds-queue/ds-queue.js
+++ ds-queue/ds-queue.js
@@ -1,14 +1,20 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
 var Queue = function() {
-  // this._storage =
+  this._front = 0;
+  this._rear = 0;
+  this._count = 0;
+  this._storage = {};
 };
 
-Queue.prototype.add = function(
-) {
+Queue.prototype.add = function(value) {
+  this._storage[this._count] = value;
+  this._rear += 1;
+  this._count += 1;
 };
 
 Queue.prototype.remove = function() {
+  return this._storage[this._front++];
 };
 
 module.exports = Queue;
