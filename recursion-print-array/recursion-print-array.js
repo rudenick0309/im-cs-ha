987e28d52476c77f76001cfa390c70e7afd5acf0
rudenick0309 <rudenick0309@gmail.com>
2020-04-01 14:39:46 +0900

    recursion cpt

--- recursion-print-array/recursion-print-array.js
+++ recursion-print-array/recursion-print-array.js
@@ -1,6 +1,15 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
-var printArray = function() {
+
+var printArray = function(num) {
+  //num = [1,2, , [3,4], 5]
+  num.forEach(function(e) {
+    if (typeof e === "number") {
+      console.log(e);
+    } else if (Array.isArray(e)) {
+      printArray(e);
+    }
+  });
 };
 
 module.exports = printArray;
