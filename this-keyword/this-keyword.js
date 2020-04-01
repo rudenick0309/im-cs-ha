08949ccdee094c0644a2e4c0584e37c485323dc5
rudenick0309 <rudenick0309@gmail.com>
2020-04-01 14:40:32 +0900

    this-keyword/

--- this-keyword/this-keyword.js
+++ this-keyword/this-keyword.js
@@ -4,14 +4,21 @@
 // returns ["Lebron James plays basketball", "Kevin Durant plays basketball"]
 
 var sport = {
-  name: 'basketball',
+  name: "basketball",
   players: [
-    { name: 'LeBron James', age: 31 },
-    { name: 'Kevin Durant', age: 28 }
+    { name: "LeBron James", age: 31 },
+    { name: "Kevin Durant", age: 28 }
   ],
-  playerNames: function () {
+  playerNames: function() {
     // YOUR CODE HERE
+    let newA = [];
+    for (let i = 0; i < this.players.length; i += 1) {
+      newA.push(`${this.players[i].name} plays ${this.name}`);
+    }
+    return newA;
   }
 };
+
+// this.player[0].name = sport.players[0].name;
 
 module.exports = sport;
